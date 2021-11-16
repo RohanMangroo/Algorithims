e86aff5e506e5f5981812cd350e06c5ab0e194a5
Seeram Rohan Mangroo <blue22387@gmail.com>
2021-11-16 17:01:17 -0500

    added heightBalancedBinaryTree

---
+++ BinaryTrees/heightBalancedBinaryTree.js
@@ -0,0 +1,33 @@
+//Given a Binary Tree return a boolean if the tree is height balanced.
+//A tree is height balanced if for each node the difference between it's left subtree height and right subtree is no greater than 1.
+
+//This problem is very similar to the diameter problem
+//Traverse down the tree, checking the difference between leftHeight and rightHeight and return the max(left, right) +1
+
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function heightBalancedBinaryTree(tree) {
+  const result = { isBalanced: true };
+  helperFunction(tree, result);
+  return result.isBalanced;
+}
+
+function helperFunction(node, result) {
+  if (!node) return 0;
+
+  if (result.isBalanced === true) {
+    const leftHeight = helperFunction(node.left, result);
+    const rightHeight = helperFunction(node.right, result);
+
+    const heightDiff = Math.abs(leftHeight - rightHeight);
+    if (heightDiff > 1) result.isBalanced = false;
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+}
