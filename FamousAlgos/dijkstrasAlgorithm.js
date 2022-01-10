755470706e153ca26a07b831e81e07fb9eb6576d
Seeram Rohan Mangroo <blue22387@gmail.com>
2022-01-10 13:49:33 -0500

    added a different version of dijkstras, a little bit better but a lot more work needed for better understanding

--- FamousAlgos/dijkstrasAlgorithm.js
+++ FamousAlgos/dijkstrasAlgorithm.js
@@ -30,3 +30,52 @@ function helperFunction(
     helperFunction(edges, minDistances, nextNode, newDistance, newSeen);
   }
 }
+
+function dijkstrasAlgorithm2(start, edges) {
+  const minDistances = new Array(edges.length).fill(Infinity);
+  minDistances[start] = 0;
+  const visited = new Set();
+
+  let currentNode = start;
+
+  while (visited.size !== edges.length) {
+    const outBoundNodes = edges[currentNode];
+
+    for (let i = 0; i < outBoundNodes.length; i++) {
+      const [node, distance] = outBoundNodes[i];
+      if (visited.has(node)) continue;
+      minDistances[node] = Math.min(
+        minDistances[node],
+        distance + minDistances[currentNode]
+      );
+    }
+
+    visited.add(currentNode);
+    const nextNode = getCurrentMinDistance(
+      visited,
+      minDistances,
+      minDistances[currentNode]
+    );
+
+    currentNode = nextNode;
+  }
+
+  return minDistances.map((num) => {
+    return num < Infinity ? num : -1;
+  });
+}
+
+function getCurrentMinDistance(visited, minDistances, currentMin) {
+  let index = 0;
+  let num = Infinity;
+
+  for (let i = 0; i < minDistances.length; i++) {
+    if (visited.has(i)) continue;
+
+    if (minDistances[i] <= num) {
+      num = minDistances[i];
+      index = i;
+    }
+  }
+  return index;
+}
