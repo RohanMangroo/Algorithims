435b5c4a7bd26cf821f4b3d4693d8fcad0373147
Seeram Rohan Mangroo <blue22387@gmail.com>
2022-02-11 08:05:39 -0500

    will be working on binary trees this week

--- BinaryTrees/maxPathSum.js
+++ BinaryTrees/maxPathSum.js
@@ -1,23 +1,32 @@
+/**
+ * Find the maxPath in the Binary Tree
+ * Keep track of the max branch(node.value + value returned from it's child)
+ * Keep track of the max path(remember node values can be negative, so the max path could be the sum of only 2 nodes and not all 3)
+ * Track the maxPath globally
+ * Return maxBranch
+ */
+
 function maxPathSum(tree) {
   const result = { maxPath: -Infinity };
-  findMax(tree, result);
+  helperFunction(tree, result);
   return result.maxPath;
 }
 
-function findMax(tree, result) {
-  if (!tree) return 0;
+function helperFunction(node, result) {
+  if (!node) return 0;
 
-  const left = findMax(tree.left, result);
-  const right = findMax(tree.right, result);
+  const left = helperFunction(node.left, result);
+  const right = helperFunction(node.right, result);
 
-  const currMaxBranch = Math.max(left, right) + tree.value;
+  const leftBranch = left + node.value;
+  const rightBranch = right + node.value;
 
-  const currPath = Math.max(
-    left + right + tree.value,
-    tree.value + left,
-    tree.value + right
+  const currentMaxPath = Math.max(
+    left + right + node.value,
+    leftBranch,
+    rightBranch
   );
-  if (currPath > result.maxPath) result.maxPath = currPath;
+  if (currentMaxPath > result.maxPath) result.maxPath = currentMaxPath;
 
-  return currMaxBranch;
+  return Math.max(leftBranch, rightBranch);
 }
