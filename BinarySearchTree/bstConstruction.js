aa43b1c58e7b8f64f7061c70a7f0f0924cde4279
Seeram Rohan Mangroo <blue22387@gmail.com>
2022-02-12 02:35:44 -0500

    added BST construction

--- BinarySearchTree/bstConstruction.js
+++ BinarySearchTree/bstConstruction.js
@@ -9,25 +9,19 @@ class BST {
   }
 
   insert(value) {
-    if (!this) return new BST(value);
-    else {
-      let currentNode = this;
-      while (currentNode) {
-        if (value < currentNode.value) {
-          if (currentNode.left === null) {
-            currentNode.left = new BST(value);
-            break;
-          } else {
-            currentNode = currentNode.left;
-          }
-        } else {
-          if (currentNode.right === null) {
-            currentNode.right = new BST(value);
-            break;
-          } else {
-            currentNode = currentNode.right;
-          }
-        }
+    let currentNode = this;
+
+    while (currentNode) {
+      if (currentNode.value > value) {
+        if (!currentNode.left) {
+          currentNode.left = new BST(value);
+          break;
+        } else currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = new BST(value);
+          break;
+        } else currentNode = currentNode.right;
       }
     }
     return this;
@@ -35,13 +29,71 @@ class BST {
 
   contains(value) {
     let currentNode = this;
+
     while (currentNode) {
       if (currentNode.value === value) return true;
-      else {
-        if (value < currentNode.value) currentNode = currentNode.left;
-        else currentNode = currentNode.right;
+
+      if (currentNode.value > value) currentNode = currentNode.left;
+      else currentNode = currentNode.right;
+    }
+
+    return false;
+  }
+
+  remove(value, root = this, currParent = null) {
+    const [nodeToRemove, parent] = this.findNodeToRemove(
+      value,
+      root,
+      currParent
+    );
+
+    if (nodeToRemove.left && nodeToRemove.right) {
+      nodeToRemove.value = this.findRightSubTreeMin(nodeToRemove.right);
+      this.remove(nodeToRemove.value, nodeToRemove.right, nodeToRemove);
+    } else if (parent === null) this.reassignPointers(nodeToRemove);
+    else if (parent.left === nodeToRemove) {
+      parent.left = nodeToRemove.left ? nodeToRemove.left : nodeToRemove.right;
+    } else if (parent.right === nodeToRemove) {
+      parent.right = nodeToRemove.left ? nodeToRemove.left : nodeToRemove.right;
+    }
+
+    return this;
+  }
+
+  reassignPointers(node) {
+    console.log(node.value);
+    if (node.left) {
+      node.value = node.left.value;
+      node.right = node.left.right;
+      node.left = node.left.left;
+    } else if (node.right) {
+      node.value = node.right.value;
+      node.left = node.right.left;
+      node.right = node.right.right;
+    }
+  }
+
+  findRightSubTreeMin(node) {
+    let currentNode = node;
+    while (currentNode.left) currentNode = currentNode.left;
+    return currentNode.value;
+  }
+
+  findNodeToRemove(value, root, parent) {
+    let currentNode = root;
+
+    while (currentNode) {
+      if (currentNode.value === value) return [currentNode, parent];
+
+      if (currentNode.value > value) {
+        parent = currentNode;
+        currentNode = currentNode.left;
+      } else {
+        parent = currentNode;
+        currentNode = currentNode.right;
       }
     }
-    return false;
+
+    return [null, null];
   }
 }
